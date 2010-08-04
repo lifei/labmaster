ba462b238674691d4abdba35a611d613d5cf0ccf
lifei <lifei.job@gmail.com>
2010-08-04 18:36:02 +0800

    实现了WeeklyReport定义

--- grails-app/domain/labmaster/weekly/WeeklyReport.groovy
+++ grails-app/domain/labmaster/weekly/WeeklyReport.groovy
@@ -1,7 +1,23 @@
 package labmaster.weekly
 
 class WeeklyReport {
+    // 实体定义 <++>
+
+    // 实体字段 <++>
+    /** 年份 */
+    Integer year
+
+    /** 星期数 */
+    Integer week
+    // <-->
 
     static constraints = {
+        year(min:2009,max:2010)
+        week(min:1,max:60)
     }
+
+    // <-->
 }
+
+// vim600: ts=4 st=4 foldmethod=marker foldmarker=<++>,<--> syn=groovy
+// vim600: encoding=utf-8 commentstring=//\ %s
