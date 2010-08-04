9709334eba66569080692daba75e6917335b6732
lifei <lifei.job@gmail.com>
2010-08-04 18:19:40 +0800

    test for new domain

---
+++ test/unit/labmaster/weekly/WeeklyReportTests.groovy
@@ -0,0 +1,17 @@
+package labmaster.weekly
+
+import grails.test.*
+
+class WeeklyReportTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
